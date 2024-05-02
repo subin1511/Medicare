7cafe07eed515588c1638b611905c90e45701aa3
subin1511 <subinkrishnana005@gmail.com>
2024-05-03 00:47:35 +0530

    mongoId

--- backend/controllers/iotCrudController.js
+++ backend/controllers/iotCrudController.js
@@ -1,9 +1,10 @@
 const Iot = require("../models/iotCrudModels");
+const mongoose = require("mongoose");
 
 
 const postTempData = async (req, res) => {
   try {
-    const temp = await Iot.updateOne({_id:"6633e11555fc84b80244a555"},{ $set: req.body })
+    const temp = await Iot.updateOne({_id:new mongoose.Types.ObjectId("6633e52dd15e5fa72fab2ec6")},{ $set: req.body })
     console.log(temp)
     res.json({"meassage":"successayada poori subine"});
   } catch (error) {
