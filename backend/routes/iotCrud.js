db0120ffa1aeccdbfc2e2a9fdcf812dfb21ff45a
subin1511 <subinkrishnana005@gmail.com>
2024-05-03 00:35:54 +0530

    updated temp route

--- backend/routes/iotCrud.js
+++ backend/routes/iotCrud.js
@@ -2,10 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
-router.post('/temperature', (req, res) => {
-    currentTemperature = req.body;
-    console.log('Received temperature:', currentTemperature);
-    res.sendStatus(200);
-  });
+const iotCrud = require("../controllers/iotCrudController");
+
+
+router.post("/temperature", iotCrud.postTempData);
+
 
 module.exports = router;
