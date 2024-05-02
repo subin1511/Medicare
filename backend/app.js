9919335890a6196fecbd1d81aeffc3c908a70a83
subin1511 <subinkrishnana005@gmail.com>
2024-05-02 17:34:13 +0530

    server to app rename

--- backend/app.js
+++ backend/app.js
@@ -5,15 +5,15 @@ const app = express();
 // require("dotenv").config();
 
 const port = 8080;
-const dbconnection = require("./utilis/db");
+const dbconnection = require("./utilis/db.js");
 
-const userroutes = require("./routes/userRoutes");
-const profileroutes = require("./routes/profileRoutes");
-const prescriptionroutes = require("./routes/prescriptionRoutes");
-const doctorroutes = require("./routes/doctorRoutes");
-const medicroutes = require("./routes/medicRoutes");
-const dashboardroutes = require("./routes/DashboardRoutes");
-const patientroutes = require("./routes/patientRoutes");
+const userroutes = require("./routes/userRoutes.js");
+const profileroutes = require("./routes/profileRoutes.js");
+const prescriptionroutes = require("./routes/prescriptionRoutes.js");
+const doctorroutes = require("./routes/doctorRoutes.js");
+const medicroutes = require("./routes/medicRoutes.js");
+const dashboardroutes = require("./routes/DashboardRoutes.js");
+const patientroutes = require("./routes/patientRoutes.js");
 const AppointmentRoute = require("./routes/AppointmentRoute.js");
 const InvoiceRoute = require("./routes/InvoiceRoute.js");
 const iotCrud = require("./routes/iotCrud.js");
@@ -36,7 +36,7 @@ app.use("/api", InvoiceRoute);
 app.use("/api", iotCrud);
 
 
-app.use("/api/paypal", require("./routes/api/paypal"));
+app.use("/api/paypal", require("./routes/api/paypal.js"));
 
 dbconnection;
 
