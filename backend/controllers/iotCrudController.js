7ceb85471387734f62ce3adcbabb8078bffc16b3
Subin Krishnan <126809043+subin1511@users.noreply.github.com>
2024-05-10 03:10:43 +0530

    new change.js

--- backend/controllers/iotCrudController.js
+++ backend/controllers/iotCrudController.js
@@ -6,7 +6,6 @@ const postTempData = async (req, res) => {
   try {
     console.log(req.body)
     const temp = await Iot.updateOne({_id:new mongoose.Types.ObjectId("6633e52dd15e5fa72fab2ec6")},{ $set: req.body })
-    console.log(temp)
     res.json({"meassage":"successfully updated temperature and pulse "});
   } catch (error) {
     res.status(404).json({ message: error.message });
